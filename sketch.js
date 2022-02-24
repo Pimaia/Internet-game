e12a60b330a6ab7c3ed0d305650a511bb7e82385
Pimaia <97753929+Pimaia@users.noreply.github.com>
2022-02-24 13:46:33 -0300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,12 +1,21 @@
 var internet, internetmovel;
 var areia, areiamovedica, homeminvisivel;
 var algodaodoce, algodaodocecolorido; 
+var obs1, obs2, obs3, obs4, obs5, obs6;
+var placar;
 
 function preload(){
 
 internetmovel = loadAnimation("trex1.png","trex3.png","trex4.png");
 areiamovedica = loadImage("ground2.png");
 algodaodocecolorido = loadImage("cloud.png");
+
+obs1 = loadImage ("obstacle1.png");
+obs2 = loadImage ("obstacle2.png");
+obs3 = loadImage ("obstacle3.png");
+obs4 = loadImage ("obstacle4.png");
+obs5 = loadImage ("obstacle5.png");
+obs6 = loadImage ("obstacle6.png");
 }
 
 function setup(){
@@ -23,6 +32,8 @@ areia.x = areia.width/2;
 
 homeminvisivel = createSprite (200, 190, 400, 10);
 homeminvisivel.visible = false;
+
+placar = 0; 
 
 borda = createEdgeSprites();
 
@@ -44,14 +55,17 @@ if(areia.x < 0){
 }
 
 if(keyDown("space")&& internet.y > 150){
-    internet.velocityY = -10;
+    internet.velocityY = -12;
 }
 internet.velocityY += 1;
 internet.collide(homeminvisivel);
 
 cloud();
+enemy();
 
 drawSprites();
+text ("Pontuação:"+ placar, 500, 50);
+placar += Math.round(frameCount/60); 
 }
 
 function cloud(){
@@ -62,6 +76,31 @@ function cloud(){
         algodaodoce.velocityX = -3;
         algodaodoce.depth = internet.depth; 
         internet.depth += 1;
+        algodaodoce.lifetime = 250;
     }
     
+}
+function enemy(){
+    if (frameCount % 60 === 0){
+        var cactus = createSprite (600, 165, 10, 40);
+        cactus.velocityX = -6;
+    var number = Math.round(random(1,6)); 
+    switch (number){
+        case 1: cactus.addImage(obs1);
+        break;
+        case 2: cactus.addImage(obs2);
+        break;
+        case 3: cactus.addImage(obs3);
+        break;
+        case 4: cactus.addImage(obs4);
+        break;
+        case 5: cactus.addImage(obs5);
+        break;
+        case 6: cactus.addImage(obs6);
+        break;
+        default: break;
+    }
+    cactus.scale = 0.5;
+    cactus.lifetime = 250;
+}
 }
